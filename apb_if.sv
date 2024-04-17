0516a60e5c34c50cf849dca8401057ba2b69cf03
RAKSHITHA R NAYAK <73732585+rakshitharnayak@users.noreply.github.com>
2024-04-17 12:44:09 +0530

    Create apb_if.sv

---
+++ apb_if.sv
@@ -0,0 +1,12 @@
+interface apb_if (input bit clk, input bit rst);
+  //input
+  logic Data_in, Valid_in;
+  logic [31:0] paddr;
+  logic [31:0] p_wdata;
+  logic p_write;
+  logic psel;
+  logic pen;
+  //output
+  logic out_port1, out_port2, out_port3, out_port4, Valid_out;
+  logic [31:0] prdata;
+endinterface
