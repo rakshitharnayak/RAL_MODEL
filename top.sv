dae981ac2b947152a86d6f300f98de8a00aa7c4b
RAKSHITHA R NAYAK <73732585+rakshitharnayak@users.noreply.github.com>
2024-04-22 20:49:37 +0530

    Updated top.sv - included design file, rst delay, clean up code

--- top.sv
+++ top.sv
@@ -15,6 +15,7 @@
 `include "reg_env.sv"
 `include "my_env.sv"
 `include "test.sv"
+`include "switch.v"
 
 module top;
 bit clk;
@@ -23,31 +24,34 @@ always #5 clk=~clk;
 
 initial begin
 rst=0;
-#5 rst=1;
+#10 rst=1;
 end
 
   apb_if apb_if(clk,rst);
 
   switch sw(.clk(apb_if.clk),
-            .rst(apb_if.rst),.data_in   (apb_if.Data_in),
-             .valid_in(apb_if.Valid_in),
+            .rst(apb_if.rst),.data_in(apb_if.Data_in),
+            .valid_in(apb_if.Valid_in),
             .valid_out(apb_if.Valid_out),
-            .paddr   (apb_if.paddr),
-            .psel    (apb_if.psel),
-            .pen (apb_if.pen),
-            .p_write  (apb_if.p_write),
-            .p_wdata  (apb_if.p_wdata),
-            .prdata  (apb_if.prdata),
-  .out_port1(apb_if.out_port1),.out_port2(apb_if.out_port2),.out_port3(apb_if.out_port3), .out_port4(apb_if.out_port4));
+            .paddr(apb_if.paddr),
+            .psel(apb_if.psel),
+            .pen(apb_if.pen),
+            .p_write(apb_if.p_write),
+            .p_wdata(apb_if.p_wdata),
+            .prdata(apb_if.prdata),
+            .out_port1(apb_if.out_port1),
+            .out_port2(apb_if.out_port2),
+            .out_port3(apb_if.out_port3),
+            .out_port4(apb_if.out_port4));
 
 initial begin
 uvm_config_db #(virtual apb_if)::set(null, "uvm_test_top.*","apb_if",apb_if);
-// run_test("reg_test");
   run_test("base_test");
 end
 
 initial begin
 $dumpfile("dump.vcd");
 $dumpvars;
+   #500 $finish;
 end
 endmodule:top
