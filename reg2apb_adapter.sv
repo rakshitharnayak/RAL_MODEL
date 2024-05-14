be61e8d1f4925c86748a6f40f33d33601d419e83
RAKSHITHA R NAYAK <73732585+rakshitharnayak@users.noreply.github.com>
2024-05-14 09:57:31 +0530

    Update reg2apb_adapter.sv

--- reg2apb_adapter.sv
+++ reg2apb_adapter.sv
@@ -1,21 +1,22 @@
 class reg2apb_adapter extends uvm_reg_adapter;
    `uvm_object_utils (reg2apb_adapter)
-//   uvm_sequencer #(bus_pkt)     tmp_seqr;
 
+  //new constructor
    function new (string name = "reg2apb_adapter");
       super.new (name);
    endfunction
 
+  //convert register to bus
    virtual function uvm_sequence_item reg2bus (const ref uvm_reg_bus_op rw);
       apb_tr pkt = apb_tr::type_id::create ("pkt");
-//      pkt.set_sequencer (this.tmp_seqr);
       pkt.write = (rw.kind == UVM_WRITE) ? 1: 0;
       pkt.addr  = rw.addr;
       pkt.data  = rw.data;
-//      `uvm_info ("adapter", $sformatf ("reg2bus addr=0x%0h data=0x%0h kind=%s", pkt.addr, pkt.data, rw.kind.name), UVM_NONE) 
+      `uvm_info ("adapter", $sformatf ("reg2bus addr=0x%0h data=0x%0h kind=%s", pkt.addr, pkt.data, rw.kind.name), UVM_HIGH) 
       return pkt; 
    endfunction
 
+  //convert bus to register
    virtual function void bus2reg (uvm_sequence_item bus_item, ref uvm_reg_bus_op rw);
       apb_tr pkt;
       if (! $cast (pkt, bus_item)) begin
@@ -25,6 +26,7 @@ class reg2apb_adapter extends uvm_reg_adapter;
       rw.kind = pkt.write ? UVM_WRITE : UVM_READ;
       rw.addr = pkt.addr;
       rw.data = pkt.data;
-//      `uvm_info ("adapter", $sformatf("bus2reg : addr=0x%0h data=0x%0h kind=%s status=%s", rw.addr, rw.data, rw.kind.name(), rw.status.name()), UVM_NONE)
+      `uvm_info ("adapter", $sformatf("bus2reg : addr=0x%0h data=0x%0h kind=%s status=%s", rw.addr, rw.data, rw.kind.name(), rw.status.name()), UVM_HIGH)
    endfunction
+  
 endclass
