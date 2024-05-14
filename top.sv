478daf41c2817c2f9ca5f7bedb018383ad4d21ee
RAKSHITHA R NAYAK <73732585+rakshitharnayak@users.noreply.github.com>
2024-05-14 10:04:26 +0530

    Update top.sv

--- top.sv
+++ top.sv
@@ -1,57 +1,65 @@
-
-
-
-   import uvm_pkg::*;
+import uvm_pkg::*;
 `include "uvm_macros.svh"
-
+`include "defines.sv"
 `include "apb_if.sv"
 `include "apb_tr.sv"
+`include "write_seq_item.sv"
 `include "ral.sv"
-`include "my_sequence.sv"
-`include "my_driver.sv"
-`include "my_monitor.sv"
-`include "my_agent.sv"
+`include "register_sequence.sv"
+`include "write_sequence.sv"
+`include "write_sequencer.sv"
+`include "register_driver.sv"
+`include "write_driver.sv"
+`include "register_monitor.sv"
+`include "write_monitor.sv"
+`include "register_agent.sv"
+`include "write_agent.sv"
+`include "read_monitor.sv"
+`include "read_agent.sv"
 `include "reg2apb_adapter.sv"
+`include "scoreboard.sv"
 `include "reg_env.sv"
-`include "my_env.sv"
+`include "env.sv"
 `include "test.sv"
-`include "switch.v"
 
 module top;
-bit clk;
-bit rst;
-always #5 clk=~clk;
+  bit clk;
+  bit rst;
+  
+  always #5 clk=~clk;
+  
+  initial begin
+    rst=0;
+    #9 rst=1;
+  end
 
-initial begin
-rst=0;
-#10 rst=1;
-end
+  apb_if vif(clk,rst);
 
-  apb_if apb_if(clk,rst);
-
-  switch sw(.clk(apb_if.clk),
-            .rst(apb_if.rst),.data_in(apb_if.Data_in),
-            .valid_in(apb_if.Valid_in),
-            .valid_out(apb_if.Valid_out),
-            .paddr(apb_if.paddr),
-            .psel(apb_if.psel),
-            .pen(apb_if.pen),
-            .p_write(apb_if.p_write),
-            .p_wdata(apb_if.p_wdata),
-            .prdata(apb_if.prdata),
-            .out_port1(apb_if.out_port1),
-            .out_port2(apb_if.out_port2),
-            .out_port3(apb_if.out_port3),
-            .out_port4(apb_if.out_port4));
-
-initial begin
-uvm_config_db #(virtual apb_if)::set(null, "uvm_test_top.*","apb_if",apb_if);
-  run_test("base_test");
-end
-
-initial begin
-$dumpfile("dump.vcd");
-$dumpvars;
-   #500 $finish;
-end
+  switch sw(.clk(vif.clk),
+            .rst(vif.rst),
+            .data_in(vif.Data_in),
+            .valid_in(vif.Valid_in),
+            .valid_out(vif.Valid_out),
+            .paddr(vif.paddr),
+            .psel(vif.psel),
+            .pen(vif.pen),
+            .p_write(vif.p_write),
+            .p_wdata(vif.p_wdata),
+            .prdata(vif.prdata),
+            .out_port1(vif.out_port1),
+            .out_port2(vif.out_port2),
+            .out_port3(vif.out_port3),
+            .out_port4(vif.out_port4));
+  
+  initial begin
+    uvm_config_db #(virtual apb_if)::set(null, "","apb_if",vif);
+    run_test("test");
+  end
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+    #10000 $finish;
+  end
+  
 endmodule:top
