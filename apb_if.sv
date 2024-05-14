dfb7dab15deab17267bcbc4418d4898e675d31f2
RAKSHITHA R NAYAK <73732585+rakshitharnayak@users.noreply.github.com>
2024-05-14 09:54:59 +0530

    Update apb_if.sv

--- apb_if.sv
+++ apb_if.sv
@@ -3,10 +3,11 @@ interface apb_if (input bit clk, input bit rst);
   logic Data_in, Valid_in;
   logic [31:0] paddr;
   logic [31:0] p_wdata;
-  logic p_write;
   logic psel;
   logic pen;
+  logic p_write;
   //output
   logic out_port1, out_port2, out_port3, out_port4, Valid_out;
   logic [31:0] prdata;
+  
 endinterface
